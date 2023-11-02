a6911fdf3c92123a86d1bc637edbe0733b7642ad
KevinGutej <76398856+KevinGutej@users.noreply.github.com>
2023-11-02 21:50:32 +0100

    get task and delete task

--- index.php
+++ index.php
@@ -6,4 +6,14 @@ if (isset($_POST['add'])) {
     $task = $_POST['task'];
     //CREATE A FUNCTION ADD TASK IN FUNCTIONS AND CALL IT OUT HERE($connection, $task);
 }
+
+$tasks = /*CREATE FUNCTION GET TASKS*/($conn);
+    while ($row = mysqli_fetch_assoc($tasks)) {
+        echo "<li>" . $row['task'] . " <a href='index.php?del=" . $row['id'] . "'>Delete</a></li>";
+    }
+
+if (isset($_GET['del'])) {
+        $id = $_GET['del'];
+    /*create function delete task*/($conn, $id);
+}
 ?>
